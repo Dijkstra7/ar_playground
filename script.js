526ce32d56a9718f4c0d1e9600b6e1a8d177b0ee
dijkstra7 <rickr.dijkd@gmail.com>
2023-06-26 15:15:04 +0200

    add local gps

--- script.js
+++ script.js
@@ -4,12 +4,34 @@ window.onload = () => {
 };
 
 function staticLoadPlaces() {
+	if ("geolocation" in navigator) {
+	  // Prompt user for permission to access their location
+	  navigator.geolocation.getCurrentPosition(
+		// Success callback function
+		(position) => {
+		  // Get the user's latitude and longitude coordinates
+		  const lat = position.coords.latitude;
+		  const lng = position.coords.longitude;
+
+		  // Do something with the location data, e.g. display on a map
+		  console.log(`Latitude: ${lat}, longitude: ${lng}`);
+		},
+		// Error callback function
+		(error) => {
+		  // Handle errors, e.g. user denied location sharing permissions
+		  console.error("Error getting user location:", error);
+		}
+	  );
+	} else {
+	  // Geolocation is not supported by the browser
+	  console.error("Geolocation is not supported by this browser.");
+	}
     return [
         {
             name: 'Magnemite',
             location: {
-                lat: 44.496470,
-                lng: 11.320180,
+                lat: lat,
+                lng: lng,
             }
         },
     ];
